29ec3c24b691fef9dd5a1cfdf539e80a02e76be1
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-02-23 18:52:28 -0500

    GE16

--- query.js
+++ query.js
@@ -13,7 +13,7 @@ const queries = [
   // What are the full names of all the registered voters whose first-name is STARR?
   Voter.find().where('firstName').equals('STARR'),
   // How many people voted in the 2016 general election (GE16)?
-  Voter.find().where('history').exists('GE16'),
+  Voter.find().where('history').exists(/GE16/i),
   // What is the last-name that comes last in the county in alphabetical order?
   Voter.find().sort('-lastName').limit(1),
   // How many zip codes does the county contain?
@@ -27,7 +27,7 @@ Promise.all(queries)
     console.log('Number of voters live in the Canton zip code (13617): ', results[0].length);
     console.log('Full names of all the registered voters whose first-name is STARR: ', results[1].map(p => p.firstName + ' ' + p.lastName));
     console.log('Number of people voted in the 2016 general election: ', results[2].length);
-    console.log('Last-name that comes last in the county in alphabetical order: ', results[3][0]);
+    console.log('Last-name that comes last in the county in alphabetical order: ', results[3][0].lastName);
     console.log('Number of zip codes the county contains: ', results[4].length);
     mongoose.connection.close();
   }).catch(error => console.error(error.stack));
